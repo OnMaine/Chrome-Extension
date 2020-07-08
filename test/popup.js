eb668e278c6650fd31fdcf184709dbf8f623a166
Володимир <onmaine@gmail.com>
2020-07-08 18:39:13 +0300

    Animation

--- test/popup.js
+++ test/popup.js
@@ -1,18 +1,17 @@
 let tb = document.querySelectorAll('.tabs__item');
+
 tb.forEach(function(userItem) {
   userItem.addEventListener('click', fTabs);
-});
+  });
 
 function fTabs(event) {
   // data-tab - номер вкладки которую нужно отобразить
   let dataTab = event.target.getAttribute('data-tab');
   // отключаю active
-
   let tabsItem = document.getElementsByClassName('tabs__item');
   for (let i = 0; i < tabsItem.length; i++) {
     tabsItem[i].classList.remove('active');
   }
-
   // все вкладки с содержимым
   let tabBody = document.getElementsByClassName('tabs__block');
   for (let i = 0; i < tabBody.length; i++) {
